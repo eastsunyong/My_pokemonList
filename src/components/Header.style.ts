b17b8adaa486e4d06e23ad4b2a60185fbe157d0b
윤선용 <sunyong9059@naver.com>
2023-08-23 18:39:25 +0900

    Feat : Header 컴포넌트 생성

---
+++ src/components/Header.style.ts
@@ -0,0 +1,20 @@
+import styled  from 'styled-components'
+
+export const Header = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    position: fixed;
+    width: 100%;
+    height: 5rem;
+    background-color: gray;
+    top: 0;
+`
+
+export const Title = styled.h2`
+    padding-left: 3rem;
+`
+
+export const Languge = styled.p`
+    padding-right: 3rem;
+`
