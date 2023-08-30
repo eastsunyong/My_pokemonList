4540dc4ec610cc97e8bb2ade01fe28a3486bfdb8
윤선용 <sunyong9059@naver.com>
2023-08-30 23:15:31 +0900

    Chore : interface 추가

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -2,3 +2,12 @@ export interface IPokemonList {
     url : string;
     name : string;
 }
+
+export interface IPokemonCard {
+    pokemonName : string;
+    pokemonUrl : string;
+}
+
+export interface IPokemonName {
+    pokemonName : string;
+}
