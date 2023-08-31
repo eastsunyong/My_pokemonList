ab73fef3c9c70e7c094aa54ce5405f8b09206ee2
윤선용 <sunyong9059@naver.com>
2023-08-31 23:38:45 +0900

    Init : PokemonType 생성

---
+++ src/components/main/PokemonType.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { IPokemonType } from 'interface'
+
+const PokemonType = (type : any) => { 
+  return (
+    <div>PokemonType</div>
+  )
+}
+
+export default PokemonType
