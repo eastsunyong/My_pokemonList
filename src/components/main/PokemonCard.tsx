74197d59efea82568cbd37f8c9d3ad7716556c4d
윤선용 <sunyong9059@naver.com>
2023-08-31 23:39:13 +0900

    Feat : PokemonCard 생성 및 스타일 추가

--- src/components/main/PokemonCard.tsx
+++ src/components/main/PokemonCard.tsx
@@ -1,18 +1,24 @@
-import React from 'react'
 import PokemonData from './PokemonData';
-import { IPokemonCard, IPokemonList } from 'interface';
+import { IPokemonList } from 'interface';
 import { useGetPokemonlist } from 'hooks/useGetPokemonlist'
+import styled from 'styled-components';
 
+const Containal = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+`
 const PokemonCard = () => {
   const { pokemonList, setPokemonList} = useGetPokemonlist();
   return (
-    <>
+    <Containal>
       {
         pokemonList.map((pokemon : IPokemonList) => (
           <PokemonData key={pokemon.url} pokemonName={pokemon.name} />
         ))
       }
-    </>
+    </Containal>
   )
 }
 
