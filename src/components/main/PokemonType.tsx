0a8df28aeea7947b41df687f5ae4c4430e830472
윤선용 <sunyong9059@naver.com>
2023-09-05 00:16:28 +0900

    Feat : PokemonType 기능 추가

--- src/components/main/PokemonType.tsx
+++ src/components/main/PokemonType.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import { IPokemonType } from 'interface'
+import { ConvertedText } from 'hooks/useTypeChange'
+import * as S from './PokemonType.style'
 
-const PokemonType = (type : any) => { 
+interface IPropsData {
+  type: IPokemonType
+}
+const PokemonType = ({ type }: IPropsData) => {
   return (
-    <div>PokemonType</div>
-  )
+      <S.Button bgColor={type?.type?.name}
+        key={type?.type?.name}>
+        {type?.type?.name !== "unknown" &&
+          type?.type?.name !== "shadow" && (
+            <S.Img
+              key={type?.type?.name}
+              src={`/images/pokemon-type-images/${type?.type?.name}.svg`}
+              alt={type?.type?.name}
+            />
+          )}
+        <S.Span>{ConvertedText[type?.type?.name]}</S.Span>
+      </S.Button>
+  );
 }
 
 export default PokemonType
