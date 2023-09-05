2378f37dd61af4606493671685344b90fc93e802
윤선용 <sunyong9059@naver.com>
2023-09-05 23:26:04 +0900

    Refactor :  훅 교체

--- src/components/main/PokemonCard.tsx
+++ src/components/main/PokemonCard.tsx
@@ -2,6 +2,7 @@ import PokemonData from './PokemonData';
 import { IPokemonList } from 'interface';
 import { useGetPokemonlist } from 'hooks/useGetPokemonlist'
 import styled from 'styled-components';
+import { useInfinite } from 'hooks/useInfinite';
 
 const Containal = styled.div`
   display: flex;
@@ -12,10 +13,18 @@ const Containal = styled.div`
 `
 const PokemonCard = () => {
   const { pokemonList, setPokemonList} = useGetPokemonlist();
+  const { pokemonListAll, pokemonListAllStatus} = useInfinite();
+  console.log(pokemonListAll?.pages[0]);
+  
   return (
     <Containal>
-      {
+      {/* {
         pokemonList.map((pokemon : IPokemonList) => (
+          <PokemonData key={pokemon.url} pokemonName={pokemon.name} />
+        ))
+      } */}
+      {
+        pokemonListAll?.pages[0]?.results.map((pokemon : IPokemonList) => (
           <PokemonData key={pokemon.url} pokemonName={pokemon.name} />
         ))
       }
