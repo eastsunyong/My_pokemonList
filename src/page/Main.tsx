84738a1f47b802ad7c341a48db9fffa499bac968
윤선용 <sunyong9059@naver.com>
2023-09-12 18:46:00 +0900

    Refactor : 코드 수정

--- src/page/Main.tsx
+++ src/page/Main.tsx
@@ -1,15 +1,23 @@
-import React, { useEffect, useState } from 'react'
 import Header from 'components/main/Header'
 import Input from 'components/main/Input'
 import PokemonCard from 'components/main/PokemonCard'
 import * as S from "./Main.style"
+import { useInfinite } from 'hooks/useInfinite'
+import { useInView } from 'react-intersection-observer';
+import Label from 'components/main/Label'
 
 const Main = () => {
+  const [ref, useInview] = useInView();
+  const { pokemonListAll, pokemonListAllStatus } = useInfinite(useInview);
   return (
     <S.Wrap>
       <Header />
-      <Input />
-      <PokemonCard />
+      {/* <Input /> */}
+      <Label />
+      {pokemonListAll?.pages.map((group, index) => (
+        <PokemonCard key={index} pokemons={group.results} status={pokemonListAllStatus} />
+      ))}
+      <div ref={ref}/>
     </S.Wrap>
   )
 }
