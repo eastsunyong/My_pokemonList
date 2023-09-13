b1e39282ccff7ad794372361aa4e79f46bb2f42b
윤선용 <sunyong9059@naver.com>
2023-09-13 23:32:15 +0900

    Feat : api 추가 및 주석 추가

--- src/client/api.ts
+++ src/client/api.ts
@@ -1,22 +1,33 @@
 import client from "./client";
 import { IApi } from "interface";
 
+//메인화면 포켓몬 리스트 가져오는 api
 export const getPokemonList = async ({ pageParam = 0 }: IApi) => {
-  return await client.get('/pokemon', {
+  return await client
+    .get("/pokemon", {
       params: { limit: 20, offset: pageParam },
     })
-    .then((res) => res.data)
+    .then((res) => res.data);
 };
 
-export const getPokemonInfo = async (url: string) => {
-    const data = await client.get(url).then((res) => res.data)
+//포켓몬 id 통해서 상세정보 요청하는 api
+export const getPokemonInfoWithId = async (id: string | undefined) => {
+  const data = await client.get(`/pokemon/${id}`).then((res) => res.data);
   return data;
 };
 
+//포켓몬 이름 통해서 상세정보 요청하는 api
+export const getPokemonInfo = async (url: string) => {
+  const data = await client.get(url).then((res) => res.data);
+  return data;
+};
+
+//포켓몬 타입 통해서 상세정보 요청하는 api
 export const getPokemonListWithType = async (typeId: string | undefined) => {
   return await client.get(`/type/${typeId}`).then((res) => res.data);
 };
 
+//포케몬 이름 가져오는 api
 export const getPokemonListWithSpecies = async (id: string | undefined) => {
   return await client.get(`/pokemon-species/${id}`).then((res) => res.data);
 };
