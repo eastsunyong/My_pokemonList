97382789ca907e2f7e4df04545099ff223e55921
윤선용 <sunyong9059@naver.com>
2023-09-13 23:30:18 +0900

    Design : PokemonShape 스타일 추가

---
+++ src/components/detail/PokemonShape.style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Wrap = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 4rem;
+    gap: 3rem;
+`
