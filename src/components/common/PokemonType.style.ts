295ad6a53b6a176285a5cce684fe4dfbbaf31870
윤선용 <sunyong9059@naver.com>
2023-09-14 23:00:18 +0900

    Rename : 컴포넌트 재분리

---
+++ src/components/common/PokemonType.style.ts
@@ -0,0 +1,30 @@
+import { styled } from "styled-components";
+import { BackgroundColor } from "hooks/useTypeChange";
+
+interface IPropsButton {
+    bgColor: string;
+}
+
+export const Button = styled.button<IPropsButton>`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 18.4rem;
+    border: none;
+    border-radius: 1.2rem;
+    background-color: ${((props) => BackgroundColor[props.bgColor])};
+    margin : 0 0.4rem 1rem 1rem;
+    padding: 2px 0;
+    font-family: "Galmuri14";
+`
+
+export const Img = styled.img`
+    width: 20px;
+`
+
+export const Span = styled.span`
+    color: white;
+    font-size: 1rem;
+    font-weight: 600;
+    margin-left: 0.2rem;
+`
