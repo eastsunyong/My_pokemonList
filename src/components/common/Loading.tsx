a214fbdbab28ac53e1530ab8cc13c455e326730a
윤선용 <sunyong9059@naver.com>
2023-09-15 23:46:35 +0900

    Feat : Loading 컴포넌트 기능 및 스타일 추가

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Wrap = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 2rem;
+    font-weight: 700;
+    font-family: "Galmuri14";
+    margin-top: 7rem;
+`
 
 const Loading = () => {
   return (
-    <div>
-      
-    </div>
+    <Wrap>
+      <p>불러오는 중 ... </p>
+    </Wrap>
   )
 }
 
