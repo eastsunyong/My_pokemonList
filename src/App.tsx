23a536893f0fd7c34bcd48feac48ef9a3a5bf938
윤선용 <sunyong9059@naver.com>
2023-09-17 23:33:54 +0900

    Feat : createContext 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import Router from './router/Router';
 import { QueryClientProvider, QueryClient } from 'react-query';
+ export const LanguageContext = createContext({
+    language: 'ko',
+    setLanguage: (language: string) => {}
+  })
 
 const App = () => {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
+  const [language, setLanguage] = useState<string>('ko')
   return (
-        <QueryClientProvider client={queryClient}>
-          <Router />
-        </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <LanguageContext.Provider value={{ language, setLanguage}}>
+        <Router />
+      </LanguageContext.Provider>
+    </QueryClientProvider>
   );
 }
 
