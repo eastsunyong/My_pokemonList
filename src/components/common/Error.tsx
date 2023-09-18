2d27a0e902b953e3bf8037fdc00f42f390abe459
윤선용 <sunyong9059@naver.com>
2023-09-18 15:41:29 +0900

    Feat : error 컴포넌트 기능 추가 및 스타일 추가

--- src/components/common/Error.tsx
+++ src/components/common/Error.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import styled from 'styled-components'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
 
-const Error = () => {
+const Wrap = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: 2rem;
+    font-weight: 700;
+    font-family: "Galmuri14";
+    margin-top: 7rem;
+    gap: 4rem;
+`
+interface IPropsData {
+  language: string;
+}
+
+const Error = (language: IPropsData) => {
   return (
-    <div>Error</div>
+    <Wrap>
+      <p>{language.language === 'ko' ? '데이터를 불러오는데 실패했습니다' : 'Failed to load data' }</p>
+      <LazyLoadImage
+        loading='lazy'
+        src='/images/NaNImg.jpg'
+        alt='에러 이미지'
+        width={480}
+        height={350}
+      />
+    </Wrap>
   )
 }
 
