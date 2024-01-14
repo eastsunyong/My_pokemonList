8d1e176a2ef79e2a5d7d8cc58d3e42beefecc0ae
윤선용 <sunyong9059@naver.com>
2024-01-14 20:52:04 +0900

    Refactor : 데이터 불러올시 css 깨짐 현상 수정
    데이터 로딩시 css 깨지던거 box를 main으로 옮김

--- src/components/main/PokemonCard.tsx
+++ src/components/main/PokemonCard.tsx
@@ -1,18 +1,10 @@
 import React, { useContext } from 'react';
 import { LanguageContext } from 'App';
-import styled from 'styled-components';
 import PokemonData from './PokemonData';
 import { IPokemonList } from 'interface';
 import Loading from 'components/common/Loading';
 import Error from 'components/common/Error';
 
-const Containal = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
-  padding: 0 2rem;
-`;
 interface IPropsData {
   pokemons: IPokemonList[];
   status: 'idle' | 'error' | 'loading' | 'success';
@@ -22,18 +14,15 @@ const PokemonCard = (pokemonList: IPropsData) => {
   const { language } = useContext(LanguageContext);
   return (
     <>
-      <Containal>
-        {pokemonList.status === 'loading' && <Loading language={language} />}
-        {pokemonList.status === 'error' && <Error language={language} />}
-
-        {pokemonList.status === 'success' && (
-          <>
-            {pokemonList?.pokemons.map((pokemon: IPokemonList) => (
-              <PokemonData key={pokemon.url} pokemonName={pokemon.name} />
-            ))}
-          </>
-        )}
-      </Containal>
+      {pokemonList.status === 'loading' && <Loading language={language} />}
+      {pokemonList.status === 'error' && <Error language={language} />}
+      {pokemonList.status === 'success' && (
+        <>
+          {pokemonList?.pokemons.map((pokemon: IPokemonList) => (
+            <PokemonData key={pokemon.url} pokemonName={pokemon.name} />
+          ))}
+        </>
+      )}
     </>
   );
 };
--- src/page/Main.tsx
+++ src/page/Main.tsx
@@ -4,6 +4,15 @@ import * as S from './Main.style';
 import { useInfinite } from 'hooks/useInfinite';
 import { useInView } from 'react-intersection-observer';
 import Label from 'components/main/Label';
+import styled from 'styled-components';
+
+const Containal = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  padding: 0 2rem;
+`;
 
 const Main = () => {
   const [ref, useInview] = useInView();
@@ -12,10 +21,12 @@ const Main = () => {
     <S.Wrap>
       <Header address={'main'} />
       <Label />
-      {pokemonListAll?.pages.map((group, index) => (
-        <PokemonCard key={index} pokemons={group.results} status={pokemonListAllStatus} />
-      ))}
-      <div ref={ref} />
+      <Containal>
+        {pokemonListAll?.pages.map((group, index) => (
+          <PokemonCard key={index} pokemons={group.results} status={pokemonListAllStatus} />
+        ))}
+        <div ref={ref} />
+      </Containal>
     </S.Wrap>
   );
 };
